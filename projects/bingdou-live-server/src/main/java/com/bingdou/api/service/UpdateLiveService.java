9d75b9420c4d8bba1529d2a15c49f452876c736c
gaoshan <gaoshan@haima.me>
2016-12-13 13:40:08 +0800

    modify config file

--- projects/bingdou-live-server/src/main/java/com/bingdou/api/service/UpdateLiveService.java
+++ projects/bingdou-live-server/src/main/java/com/bingdou/api/service/UpdateLiveService.java
@@ -1,10 +1,30 @@
 package com.bingdou.api.service;
 
+import com.bingdou.core.helper.BaseRequest;
+import com.bingdou.core.helper.ServiceResult;
+import com.bingdou.core.model.User;
+import com.bingdou.core.service.IMethodService;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by gaoshan on 16-11-4.
  */
 @Service
-public class UpdateLiveService {
+public class UpdateLiveService implements IMethodService {
+    @Override
+    public String getMethodName() {
+        return "update_live";
+    }
+
+    @Override
+    public ServiceResult execute4Server(HttpServletRequest request, BaseRequest baseRequest, User user) throws Exception {
+        return null;
+    }
+
+    @Override
+    public ServiceResult execute4Client(HttpServletRequest request, BaseRequest baseRequest, User user) throws Exception {
+        return null;
+    }
 }
